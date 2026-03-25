6c723ae673fbed19ad7e1794fac28ea14b735272
KurumiW <2209278137@qq.com>
2026-03-26 03:53:55 +0800

    基本功能实现

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), vue()],
+  base: "./",
   build: {
     outDir: "docs",
   },
